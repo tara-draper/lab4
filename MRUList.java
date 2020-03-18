e3df036fa5e81e24f3a946d0016e004ef3e43140
Tara Draper <taradraper@optusnet.com.au>
2020-03-18 14:56:53 -0700

    add features

--- MRUList.java
+++ MRUList.java
@@ -1,26 +1,24 @@
+
 /**
  * TODO - your description here
  *
  * @author TODO YOUR NAME HERE
  */
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.*;
 
 public class MRUList<T> extends MyLinkedList<T> {
 
     public boolean contains(Object o) {
-        // TODO - your code here
-	return false;
-    }
-    
-    public boolean add(T x) {
-        // TODO - your code here
+	// TODO - your code here
 	return false;
     }
 
-    public void add(int index, T x) {
-        // TODO - your code here
+    public boolean add(T x) {
+	super.add(0, x);
+	return true;
     }
-    
+
+    public void add(int index, T x) {
+	super.add(0, x);
+    }
+
 }
